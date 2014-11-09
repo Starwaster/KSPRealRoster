59056a74bf4da7fc124ad974241d7bb5bde5840c
fingerboxes <enneract@fingerbox.es>
2014-11-08 18:49:32 -0700

    persistence fixed

--- RealRosterCore.cs
+++ RealRosterCore.cs
@@ -11,22 +11,54 @@ namespace RealRoster
     class EditorBehaviour : MonoBehaviour
     {
         private static readonly string TAG = "EditorBehaviour";
-
-        private Part firstPod = null;
+        
+        // These flags control actions to be taken on the update following editor modification.
         private bool firstPodFlag = false;
+        private bool dirtyFrame = false;
+        private bool partsUpdated = false;
 
+        // This holds a reference to the first pod placed. This one gets automatically crewed, and this value should go null when it is removed.
+        // (Thanks Unity!)
+        private Part firstPod = null;
+
+        // This structure holds an association between Parts and their Crew configurations which the user has written
+        private Dictionary<Part , ProtoCrewMember[]> PartCrewDictionary = new Dictionary<Part, ProtoCrewMember[]>();
+
+        // Limits on the number of times we spam expensive operations
+        private int framecounter = 0;
+        private static readonly int FRAME_INTERVAL = 20;
+
+        // This happens when the behaviour is loaded.
         void Awake()
         {
             CommonLogic.DebugMessage(TAG, "Awake()");
             GameEvents.onEditorShipModified.Add(onEditorShipModified);
         }
 
+        // This triggers every frame.
         void Update()
         {
             if (firstPodFlag)
             {
+                CommonLogic.DebugMessage(TAG, "Update()");
                 CommonLogic.getDefaultManifest();
                 firstPodFlag = false;
+                updatePartsDictionary();
+            }
+            else if (EditorLogic.fetch.editorScreen == EditorLogic.EditorScreen.Parts && dirtyFrame)
+            {
+                dirtyFrame = !dirtyFrame;
+                fixManifest();
+                partsUpdated = false;
+            }
+            else if (EditorLogic.fetch.editorScreen == EditorLogic.EditorScreen.Crew && !partsUpdated)
+            {
+                updatePartsDictionary();
+                partsUpdated = true;
+            } 
+            else if (EditorLogic.fetch.editorScreen == EditorLogic.EditorScreen.Crew)
+            {
+                updateCrewDictionary();
             }
         }
 
@@ -49,6 +81,125 @@ namespace RealRoster
                         firstPodFlag = true;
                         break;
                     }
+                }
+            }
+            else
+            {
+                dirtyFrame = !dirtyFrame;
+            }            
+        }
+
+        void fixManifest()
+        {
+            CommonLogic.DebugMessage(TAG, "fixManifest()");
+            foreach (PartCrewManifest pcm in CMAssignmentDialog.Instance.GetManifest())
+            {
+                if (pcm.PartInfo.partPrefab.CrewCapacity > 0)
+                {
+                    ProtoCrewMember[] crewArray = pcm.GetPartCrew();
+                    for (int i = 0; i < crewArray.Length; i++)
+                    {
+                        if (crewArray[i] != null)
+                        {
+                            pcm.RemoveCrewFromSeat(i);
+                        }
+                    }
+                }
+            }
+
+            // Place correct Crew back
+            List<ProtoCrewMember> addedList = new List<ProtoCrewMember>();
+
+            foreach (Part part in PartCrewDictionary.Keys)
+            {
+                // We can't touch pods which are not attached
+                if (EditorLogic.fetch.ship.Contains(part))
+                {
+                    ProtoCrewMember[] group = PartCrewDictionary[part];
+
+                    //Make sure that this Kerbal isn't being cloned
+                    for (int idx = 0; idx < group.Length; idx++)
+                    {
+                        if (addedList.Contains(group[idx]))
+                        {
+                            group[idx] = null;
+                        }
+                        else
+                        {
+                            // Add this kerb to our list of people who already have a seat
+                            addedList.Add(group[idx]);
+                        }
+                    }
+                    // Perform the crewing.
+                    crewPartWithAssignment(part, group);
+                }
+            }
+        }
+
+        // Returns the PartCrewManifest that is owned by the specified Part.
+        // If the Part is not attached to the ShipConstruct, null will be returned.
+        // **REFACTOR ME?**
+        PartCrewManifest findManifestByPart(Part part)
+        {
+            ShipConstruct ship = EditorLogic.fetch.ship;
+            for (int idx = 0; idx < ship.Count; idx++)
+            {
+                if (part == ship[idx])
+                {
+                    return ShipConstruction.ShipManifest[idx];
+                }
+            }
+            return null;
+        }
+
+        // Fills the part's crew capacity with the specified crew array.
+        void crewPartWithAssignment(Part part, ProtoCrewMember[] crew)
+        {
+            PartCrewManifest manifest = findManifestByPart(part);
+
+            int limit = (part.CrewCapacity < crew.Length) ? part.CrewCapacity : crew.Length;
+            for (int idx = 0; idx < limit; idx++)
+            {
+                if (crew[idx] != null)
+                {
+                    manifest.AddCrewToSeat(crew[idx], idx);
+                }
+            }
+        }      
+
+        /* This method should do the initial population of Parts and Crews in PartCrewDictionary.
+         * It should only be called once per time the user switches to the Crew tab, as it is
+         * relatively expensive. */
+        void updatePartsDictionary()
+        {
+            CommonLogic.DebugMessage(TAG, "updatePartsDictionary()");
+            // Remove parts which don't exist in the scene anymore.
+            // This really shouldn't work at all, because Dictionaries
+            // arent's supposed to be able to have null keys.
+            // But they do! (Thanks, Unity!)
+            PartCrewDictionary = (from k in PartCrewDictionary where k.Key != null select k).ToDictionary(k => k.Key, k => k.Value);
+
+            // Scan for crewable Parts not yet in the Dictionary 
+            foreach (var part in EditorLogic.fetch.ship.Select((value, index) => new {index, value }))
+            {
+                if (part.value.CrewCapacity > 0 && !PartCrewDictionary.Keys.Contains(part.value))
+                {                    
+                    PartCrewDictionary.Add(part.value, CMAssignmentDialog.Instance.GetManifest()[part.index].GetPartCrew());
+                }
+            }
+        }
+
+        /* This method should update the Values (Crew) in the PartCrewDictionary. It does not look for new
+         * Parts, which should not be added or removed during the course of the session in the Crew tab. */
+        void updateCrewDictionary()
+        {
+            CommonLogic.DebugMessage(TAG, "updateCrewDictionary()");
+            // Scan for crewable parts not yet in our dictionary.
+            foreach (var part in EditorLogic.fetch.ship.Select((value, index) => new {index, value}))
+            {                
+                if (PartCrewDictionary.Keys.Contains(part.value))
+                {                    
+                    PartCrewDictionary[part.value] = CMAssignmentDialog.Instance.GetManifest()[part.index].GetPartCrew();
                 }
             }
         }
@@ -75,191 +226,7 @@ namespace RealRoster
             GameEvents.onGUILaunchScreenVesselSelected.Remove(onVesselSelected);
         }
     }
-
-    [KSPAddon(KSPAddon.Startup.EditorAny, false)]
-    class PersistenceBehaviour : MonoBehaviour
-    {
-        private static readonly string TAG = "PersistenceBehaviour";
-
-        // This structure holds an association between Parts and their Crew configurations which the user has written
-        Dictionary<Part, ProtoCrewMember[]> PartCrewDictionary = new Dictionary<Part, ProtoCrewMember[]>();
-
-        // This value should be set to true when the user has modified a part, maybe!
-        private bool dirtyFame = false;
-
-        private int framecounter = 1;
-
-        void Awake()
-        {
-            CommonLogic.DebugMessage(TAG, "Awake()");
-            GameEvents.onEditorShipModified.Add(onEditorShipModified);
-        }
-
-        void Update()
-        {            
-            // User is modifying the crew! Watch for crew changes and record them.
-            // Unfortunately, there is no event that we can hook here, so we get do perform costly operations
-            // nearly every frame.
-            if (EditorLogic.fetch.editorScreen == EditorLogic.EditorScreen.Crew)
-            {
-                if (++framecounter % 10 == 0)
-                {
-                    framecounter = 1;
-                    updatePartCrewDictionary();
-                }
-            }
-            // User is modifying the parts! Watch for part changes and record them.
-            else if (EditorLogic.fetch.editorScreen == EditorLogic.EditorScreen.Parts && dirtyFame)
-            {
-                CommonLogic.DebugMessage(TAG, "On EditorScreen, dealing with a dirtyFrame");
-                dirtyFame = !dirtyFame;
-
-                if (ShipConstruction.ShipManifest != null)
-                {
-                    cleanVesselManifest();
-                }
-
-                if (EditorLogic.fetch.CountAllSceneParts(true) == 0)
-                {
-                    ShipConstruction.ShipManifest = null;
-                }
-            }
-        }
-
-        void Destroy()
-        {
-            CommonLogic.DebugMessage(TAG, "Destroy()");
-            GameEvents.onEditorShipModified.Remove(onEditorShipModified);
-        }
-
-        void onEditorShipModified(ShipConstruct ship)
-        {
-            CommonLogic.DebugMessage(TAG, "OnEditorShipModified()");
-            // This should prevent persistence module from stepping on 'allowed' crewing.
-            if (ShipConstruction.ShipManifest != null)
-            {
-                dirtyFame = !dirtyFame;
-                updatePartCrewDictionary();
-            }
-        }
-
-        void updatePartCrewDictionary()
-        {
-            EditorLogic editor = EditorLogic.fetch;
-            VesselCrewManifest manifest = CMAssignmentDialog.Instance.GetManifest();
-
-            // Scan for crewable parts not yet in our dictionary
-            for (int i = 0; i < editor.ship.Count; i++)
-            {
-                Part current = editor.ship[i];
-                if (current.CrewCapacity > 0 && !PartCrewDictionary.Keys.Contains(current))
-                {
-                    CommonLogic.DebugMessage(TAG,"Adding to dictionary, part: " + current.partInfo.title + " manifest: " + manifest[i].PartInfo.title);
-                    PartCrewDictionary.Add(current, manifest[i].GetPartCrew());
-                }
-            }
-
-            // Look for parts with updated crews
-            for (int i = 0; i < editor.ship.Count; i++)
-            {
-                Part shipPart = editor.ship[i];
-                if (PartCrewDictionary.ContainsKey(shipPart))
-                {
-                    CommonLogic.DebugMessage(TAG,"Updating part in dictionary: " + manifest[i].PartInfo.title);
-                    PartCrewDictionary[shipPart] = manifest[i].GetPartCrew();
-                }
-            }
-
-            // Remove parts which don't exist in the scene anymore.
-            // the Part key has gone null. THIS IS WEIRD
-            PartCrewDictionary = (from kv in PartCrewDictionary
-                                      where kv.Key != null
-                                      select kv).ToDictionary(kv => kv.Key, kv => kv.Value);
-
-            ShipConstruction.ShipManifest = manifest;
-        }
-
-        void cleanVesselManifest()
-        {
-            CommonLogic.DebugMessage(TAG, "Cleaning vessel manifest");
-            VesselCrewManifest manifest = CMAssignmentDialog.Instance.GetManifest();
-
-            // Remove all currently assigned crew, they are incorrect
-            foreach (PartCrewManifest pcm in manifest)
-            {
-                if (pcm.PartInfo.partPrefab.CrewCapacity > 0)
-                {
-                    ProtoCrewMember [] crew = pcm.GetPartCrew();
-                    for (int i = 0; i < crew.Length; i++)
-                    {
-                        pcm.RemoveCrewFromSeat(i);
-                    }
-                }
-            }
-
-            // Place correct Crew back
-            List<ProtoCrewMember> addedList = new List<ProtoCrewMember>();
-
-            foreach (Part pod in PartCrewDictionary.Keys)
-            {
-                // We can't touch pods which are not attached
-                if (EditorLogic.fetch.ship.Contains(pod))
-                {
-                    ProtoCrewMember[] group = PartCrewDictionary[pod];
-
-                    //Make sure that this Kerbal isn't being cloned
-                    for (int idx = 0; idx < group.Length; idx++)
-                    {
-                        if (addedList.Contains(group[idx]))
-                        {
-                            group[idx] = null;
-                        }
-                        else
-                        {
-                            // Add this kerb to our list of people who already have a seat
-                            addedList.Add(group[idx]);
-                        }
-                    }
-                    // Perform the crewing.
-                    crewPartWithAssignment(pod, group);
-                }
-            }
-
-            CMAssignmentDialog.Instance.RefreshCrewLists(manifest, true, true);
-        }
-
-        // Fills the part's crew capacity with the specified crew array.
-        void crewPartWithAssignment(Part part, ProtoCrewMember[] crew)
-        {
-            PartCrewManifest manifest = findManifestByPart(part);
-
-            int limit = (part.CrewCapacity < crew.Length) ? part.CrewCapacity : crew.Length;
-            for (int idx = 0; idx < limit; idx++)
-            {
-                if (crew[idx] != null)
-                {
-                    manifest.AddCrewToSeat(crew[idx], idx);
-                }
-            }
-        }
-
-        // Returns the PartCrewManifest that is owned by the specified Part.
-        // If the Part is not attached to the ShipConstruct, null will be returned.
-        // **REFACTOR ME?**
-        PartCrewManifest findManifestByPart(Part part)
-        {
-            ShipConstruct ship = EditorLogic.fetch.ship;
-            for (int idx = 0; idx < ship.Count; idx++)
-            {
-                if (part == ship[idx])
-                {
-                    return ShipConstruction.ShipManifest[idx];
-                }
-            }
-            return null;
-        }
-    }
-
+  
     public static class CommonLogic
     {
         private static readonly string TAG = "CommonLogic";
